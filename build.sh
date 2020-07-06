4f143afd6223d32b5c47ece15ff6ec599b862525
Callum Murphy <callum.a.murphy.77@gmail.com>
2020-07-06 15:30:31 +0100

    update build script to split linux/windows binaries

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ export GOARCH=amd64
 
 echo "Building all binaries for '$GOARCH'..."
 
-GOOS=linux go build -o bin/dirtysocks src/dirtysocks/dirtysocks.go
-GOOS=windows go build -o bin/dirtysocks.exe src/dirtysocks/dirtysocks.go 
+GOOS=linux go build -o bin/linux/dirtysocks src/dirtysocks/dirtysocks.go
+GOOS=windows go build -o bin/windows/dirtysocks.exe src/dirtysocks/dirtysocks.go 
 
-GOOS=linux CGO_ENABLED=1 go build -o bin/shellcode src/shellcode/shellcode-linux.go
-GOOS=windows go build -o bin/shellcode.exe src/shellcode/shellcode-windows.go
+GOOS=linux CGO_ENABLED=1 go build -o bin/linux/shellcode src/shellcode/shellcode-linux.go
+GOOS=windows go build -o bin/windows/shellcode.exe src/shellcode/shellcode-windows.go
 
-GOOS=linux CGO_ENABLED=1 go build -o bin/shellcode-inject src/shellcode-inject/shellcode-inject-linux.go
-GOOS=windows go build -o bin/shellcode-inject.exe src/shellcode-inject/shellcode-inject-windows.go
+GOOS=linux CGO_ENABLED=1 go build -o bin/linux/shellcode-inject src/shellcode-inject/shellcode-inject-linux.go
+GOOS=windows go build -o bin/windows/shellcode-inject.exe src/shellcode-inject/shellcode-inject-windows.go
 
-GOOS=linux go build -o bin/shell-reverse src/shell-reverse/shell-reverse.go
-GOOS=windows go build -o bin/shell-reverse.exe src/shell-reverse/shell-reverse.go
+GOOS=linux go build -o bin/linux/shell-reverse src/shell-reverse/shell-reverse.go
+GOOS=windows go build -o bin/windows/shell-reverse.exe src/shell-reverse/shell-reverse.go
 
-GOOS=linux go build -o bin/smuggler src/smuggler/smuggler.go
-GOOS=windows go build -o bin/smuggler.exe src/smuggler/smuggler.go
+GOOS=linux go build -o bin/linux/smuggler src/smuggler/smuggler.go
+GOOS=windows go build -o bin/windows/smuggler.exe src/smuggler/smuggler.go
